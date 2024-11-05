3798ad1e8e3d8c4dc862a30ddc2e5d946a2826a2
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-11-06 02:04:13 +0400

    Initial Commit

---
+++ VideoPlayer Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer Prototype
+//
+//  Created by Mac Alexander on 06.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
